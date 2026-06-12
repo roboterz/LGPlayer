8e833dcdd54b4a4775b7d78070a1f318e9a0e895
roboterz <40050902+roboterz@users.noreply.github.com>
2026-06-12 00:40:10 -0400

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/aerolite/lgplayer/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/aerolite/lgplayer/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.lgplayer
+package com.aerolite.lgplayer
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -14,11 +14,11 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class aeroliteInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.lgplayer", appContext.packageName)
+        assertEquals("com.aerolite.lgplayer", appContext.packageName)
     }
 }
